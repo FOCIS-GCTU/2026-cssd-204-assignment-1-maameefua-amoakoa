75fb6bbd3bee64db31e1f1ae544d41a8fd263b76
Charity Duku Amoakoa <2425402965@live.gctu.edu.gh>
2026-05-31 19:27:55 +0000

    Update initials.py
    Prints initials CDA as large stylized block letters, each 12 characters wide and 10 characters high

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: CHARITY DUKU AMOAKOA
+# STUDENT ID: 2425402965
+# Email: 2425402965@live.gctu.edu.gh
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor,CHARITY DUKU AMOAKOA, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+ #Prints initials CDA as large stylized block letters, each 12 characters wide and 10 characters high.
 
+    print()
+    print("...CDA")
+    print()
+    print(".....CCCCCCCCCC........DDDDDDDDDDD..............AA..........")
+    print("...CCC.................DD........DD............A..A.........")
+    print("...CCC.................DD........DD...........AA..AA........")
+    print("...CCC.................DD........DD..........AA....AA.......")
+    print("...CCC.................DD........DD.........AAA....AAA......")
+    print("...CCC.................DD........DD........AAAAAAAAAAAA.....")
+    print("...CCC.................DD........DD........AA........AA.....")
+    print("...CCC.................DD........DD........AA........AA.....")
+    print("...CCC............**...DD........DD...**...AA........AA...**")
+    print(".....CCCCCCCCCC...**...DDDDDDDDDDD....**...AA........AA...**")
+    print()
 
 
 main()
